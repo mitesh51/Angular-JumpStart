d3c3bbfc3b31f7275f65aff8206d590f7bc63ad6
mitesh51 <mitesh.soni83@gmail.com>
2021-08-06 01:30:31 +0530

    Update karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -17,7 +17,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/Demo1'),
+      dir: require('path').join(__dirname, './coverage/angular-jump-start'),
       reports: ['html', 'lcovonly','cobertura'],
       fixWebpackSourcePaths: true
     },
